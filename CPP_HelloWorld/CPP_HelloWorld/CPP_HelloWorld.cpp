09a646c4dcde48a192aac703aea15a472055b5ee
jrajan14 <j.rajan14@yahoo.com>
2020-10-06 00:12:24 +0100

    Added HELLO WORLD program

---
+++ CPP_HelloWorld/CPP_HelloWorld/CPP_HelloWorld.cpp
@@ -0,0 +1,19 @@
+/*
+Basic C++ Programs
+By J RAJAN
+For Learners
+Title: HELLO WORLD
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //std::cout << "Hello World!\n";   //IF YOU DON'T WRITE "using namespace std", you need to use this line of code.
+
+    cout << "Hello World";          //Simple "cout" statement
+
+}
